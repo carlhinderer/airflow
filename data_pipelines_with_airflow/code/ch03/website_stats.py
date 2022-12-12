0f46fa7fdbe0112a05b0e6b24255f66a5e5e45df
Carl Hinderer <carl.hinderer4@gmail.com>
2022-12-11 23:47:46 -0700

    Updated chapter 3 notes.

--- data_pipelines_with_airflow/code/ch03/website_stats.py
+++ data_pipelines_with_airflow/code/ch03/website_stats.py
@@ -4,27 +4,33 @@ import pandas as pd
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
- 
+
+
 dag = DAG(
-   dag_id="user_events",
-   start_date=datetime(2015, 6, 1),
-   schedule_interval=None,
+    dag_id="user_events",
+    start_date=datetime(2015, 6, 1),
+    schedule_interval=None,
 )
- 
+
+
 fetch_events = BashOperator(
-   task_id="fetch_events",
-   bash_command="curl -o data/events.json https://localhost:5000/events",
-   dag=dag,
+    task_id="fetch_events",
+    bash_command=(
+        "mkdir -p /data && "
+        "curl -o /data/events.json "
+        "https:/ /localhost:5000/events" 
+    ),
+    dag=dag,
 )
- 
- 
+
+
 def _calculate_stats(input_path, output_path):
-   """Calculates event statistics."""
-   events = pd.read_json(input_path)
-   stats = events.groupby(["date", "user"]).size().reset_index()
-   stats.to_csv(output_path, index=False)
- 
- 
+    """Calculates event statistics."""
+    events = pd.read_json(input_path)
+    stats = events.groupby(["date", "user"]).size().reset_index()
+    stats.to_csv(output_path, index=False)
+
+
 calculate_stats = PythonOperator(
    task_id="calculate_stats",
    python_callable=_calculate_stats,
@@ -34,5 +40,6 @@ calculate_stats = PythonOperator(
    },
    dag=dag,
 )
- 
+
+
 fetch_events >> calculate_stats
